71d34ba8187a78702d28e1576713dbd8ea085e9b
Orijma <remymarquentin@gmail.com>
2021-04-10 13:49:31 +0200

    QuestTwig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,4 +23,9 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/index.html.twig');
     }
+    public function showProduct()
+    {
+        $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre'];
+        return $this->twig->render('Home/products.html.twig', ['products' => $products]);
+    }
 }
